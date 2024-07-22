53fe070963c097f5d5cec376e3be4f02d793299a
Nikita <itstudiolinux@gmail.com>
2024-07-22 14:55:17 +0300

    добавил классы с наследованием, но не могу понять как добавить подсчет

--- homework1.py
+++ homework1.py
@@ -1,11 +1,25 @@
 class Student:
-    def __init__(self, name, surname, gender):
+    def __init__(self, name, surname, courses):
         self.name = name
         self.surname = surname
-        self.gender = gender
+        self.courses = courses
         self.finished_courses = []
         self.courses_in_progress = []
         self.grades = {}
+
+    def rate_hw(self, lecturer, course, grade):
+        if isinstance(lecturer,
+                      Lecturer) and course in self.courses_attached and course in lecturer.courses_in_progress:
+            if course in student.grades:
+                lecturer.grades[course] += [grade]
+            else:
+                lecturer.grades[course] = [grade]
+        else:
+            return 'Ошибка'
+
+    def __str__(self):
+        return (f'Имя: {self.name} \nФамилия: {self.surname} \nСредняя оценка за домашние задания: 9.9 '
+                f'\nКурсы в процессе изучения: {self.courses} \nЗавершенные курсы: Введение в программирование')
 
 
 class Mentor:
@@ -14,24 +28,55 @@ class Mentor:
         self.surname = surname
         self.courses_attached = []
 
-    def say_mentor(self):
-        print(f'Привет всем меня зовут {self.surname} {self.name}, и я Наставник Ха-ха-ха')
 
+class Lecturer(Mentor):
+    def __init__(self, name, surname):
+        super().__init__(name, surname)
+        self.name = name
+        self.surname = surname
+        self.courses_attached = []
+        self.courses_in_progress = []
 
-class Lecture(Mentor):
-    def say_lecture(self):
-        print(f'Привет меня зовут {self.surname} {self.name} и я Лектор')
+    def __str__(self):
+        return f'Имя: {self.name} \nФамилия: {self.surname} \nСредняя оценка за лекции: 9.9 \n'
 
 
 class Reviewer(Mentor):
-    def say_reviewer(self):
-        print(f'Привет меня зовут {self.surname} {self.name} и я Эксперт в области программирования')
+    def __init__(self, name, surname):
+        super().__init__(name, surname)
+        self.courses_attached = []
+        self.courses_in_progress = []
+
+    def rate_hw(self, student, course, grade):
+        if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
+            if course in student.grades:
+                student.grades[course] += [grade]
+            else:
+                student.grades[course] = [grade]
+        else:
+            return 'Ошибка'
+
+    def __str__(self):
+        return f'Имя: {self.name} \nФамилия: {self.surname} \n'
 
 
-mentors = Mentor("Никита", "Добров-Рыжов")
-lectures = Lecture("Анна", "Доброва-Рыжова")
-reviewers = Reviewer("Марк", "Добров-Рыжов")
+some_reviewer = Reviewer('Some', 'Buddy')
+some_reviewer.courses_in_progress += ['Python', 'Git']
+some_lecturer = Lecturer('Some', 'Buddy')
+some_student = Student('Ruoy', 'Eman', 'Python')
 
-mentors.say_mentor()
-lectures.say_lecture()
-reviewers.say_reviewer()
+# best_student = Student('Ruoy', 'Eman', 'your_gender')
+# best_student.courses_in_progress += ['Python']
+
+
+# cool_mentor.courses_attached += ['Python']
+
+# cool_mentor.rate_hw(best_student, 'Python', 10)
+# cool_mentor.rate_hw(best_student, 'Python', 10)
+# cool_mentor.rate_hw(best_student, 'Python', 10)
+#
+# print(best_student.grades)
+
+print(some_reviewer)
+print(some_lecturer)
+print(some_student)
